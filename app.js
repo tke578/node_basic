1ab90272607b5a299487c27eb0e40bb6f6d21fb2
Adnrew G <tke578@gmail.com>
2014-06-26 00:21:37 -0700

    Completed

--- app.js
+++ app.js
@@ -10,18 +10,48 @@ server = http.createServer(function (request, response){
       response.write(contents); 
       response.end();
     });
-  } else if(request.url === '/dojo.html'){
-    fs.readFile('views/dojo.html', 'utf8', function (errors, contents){
+  } else if(request.url === '/cats.html'){
+    fs.readFile('views/cats.html', 'utf8', function (errors, contents){
       response.write(contents);
       response.end();
     });
+  }else if(request.url === '/new_cars.html'){
+    fs.readFile('views/new_cars.html', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
   }else if(request.url === '/images/download.jpg'){
     fs.readFile('./images/download.jpg', function(errors, contents){
      response.write(contents);
      response.end();
     })
-  } else {
-    response.end('File not found!!!');
+  } else if(request.url === '/images/image_1.jpg'){
+    fs.readFile('./images/image_1.jpg', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
+  } else if(request.url === '/images/images_2.jpg'){
+    fs.readFile('./images/images_2.jpg', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
+  } else if(request.url === '/images/cat_1.jpg'){
+    fs.readFile('./images/cat_1.jpg', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
+  }else if(request.url === '/images/cat_2.jpg'){
+    fs.readFile('./images/cat_2.jpg', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
+  }else if(request.url === '/images/cat_3.jpg'){
+    fs.readFile('./images/cat_3.jpg', function(errors, contents){
+     response.write(contents);
+     response.end();
+    })
+  }else {
+    response.end('What are you looking for?');
   }
 });
 server.listen(8000);
