486af8ee3369408101fcbcb9d0be067b73c05ae6
Adnrew G <tke578@gmail.com>
2014-06-29 15:17:27 -0700

    Trying to fix module

--- app.js
+++ app.js
@@ -1,58 +1,11 @@
 //http server
 var http = require('http');
 var fs = require('fs');
+
+var static_contents = require('./node_modules/static.js');
 //creating a server
 server = http.createServer(function (request, response){
-  response.writeHead(200, {'Content-type': 'text/html'});
-  console.log('Request', request.url);
-  if(request.url === '/'){
-    fs.readFile('views/cars.html', 'utf8', function (errors, contents){
-      response.write(contents); 
-      response.end();
-    });
-  } else if(request.url === '/cats.html'){
-    fs.readFile('views/cats.html', 'utf8', function (errors, contents){
-      response.write(contents);
-      response.end();
-    });
-  }else if(request.url === '/new_cars.html'){
-    fs.readFile('views/new_cars.html', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  }else if(request.url === '/images/download.jpg'){
-    fs.readFile('./images/download.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  } else if(request.url === '/images/image_1.jpg'){
-    fs.readFile('./images/image_1.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  } else if(request.url === '/images/images_2.jpg'){
-    fs.readFile('./images/images_2.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  } else if(request.url === '/images/cat_1.jpg'){
-    fs.readFile('./images/cat_1.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  }else if(request.url === '/images/cat_2.jpg'){
-    fs.readFile('./images/cat_2.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  }else if(request.url === '/images/cat_3.jpg'){
-    fs.readFile('./images/cat_3.jpg', function(errors, contents){
-     response.write(contents);
-     response.end();
-    })
-  }else {
-    response.end('What are you looking for?');
-  }
+  static_contents(request, response);  //this will serve all static files automatically
 });
 server.listen(8000);
 console.log("Running in localhost at port 8000");
